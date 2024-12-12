2e57ecd2f982d2fb318eb6c21a0af3997a3eb948
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2024-12-12 15:24:41 -0300

    tokens

--- frontend/src/pages/TokensPage.js
+++ frontend/src/pages/TokensPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom"; // Importar o hook useNavigate
 
 function TokensPage() {
   const [tokens, setTokens] = useState([]);
   const [error, setError] = useState(null);
   const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+  const navigate = useNavigate(); // Criar o objeto navigate
 
   useEffect(() => {
     const fetchTokens = async () => {
@@ -19,8 +21,26 @@ function TokensPage() {
     fetchTokens();
   }, [apiBaseUrl]);
 
+  const handleBack = () => {
+    navigate(-1); // Navegar para a página anterior
+  };
+
   return (
     <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto" }}>
+      <button
+        onClick={handleBack}
+        style={{
+          marginBottom: "20px",
+          padding: "10px 20px",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Voltar
+      </button>
       <h2 style={{ textAlign: "center", marginBottom: "20px" }}>
         Tokens Registrados
       </h2>
