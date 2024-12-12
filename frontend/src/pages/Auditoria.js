5d5f49fc87be21175898f30dc6dae602c2fb53a2
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2024-12-12 15:49:16 -0300

    audi

--- frontend/src/pages/Auditoria.js
+++ frontend/src/pages/Auditoria.js
@@ -2,26 +2,27 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import AuditPage from "../components/AuditPage";
 import LogoutButton from "../components/LogoutButton";
-
-
-const handleLogout = async (navigate) => {
-  try {
-    await logout();
-    navigate("/login");
-  } catch (error) {
-    console.error("Erro ao realizar logout: ", error);
-  }
-};
+import { useAuth } from "../context/AuthContext";
 
 function Auditoria() {
+  const { logout } = useAuth(); // Obtém a função logout do contexto de autenticação
   const navigate = useNavigate();
 
   const handleBack = () => {
     navigate(-1); // Volta para a página anterior
   };
 
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/login");
+    } catch (error) {
+      console.error("Erro ao realizar logout: ", error);
+    }
+  };
+
   return (
-    <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto" }}>
+    <div style={{ padding: "20px", maxWidth: "800px", margin: "0 auto" }}>
       <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "20px" }}>
         <button
           onClick={handleBack}
@@ -36,7 +37,7 @@ function Auditoria() {
         >
           Voltar
         </button>
-        <LogoutButton handleLogout={() => handleLogout(navigate)} />
+        <LogoutButton handleLogout={handleLogout} />
       </div>
       <AuditPage />
     </div>
