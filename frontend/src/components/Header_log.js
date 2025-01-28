fb032db43c2e4f75030cccb1524b37ff55b3dd48
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2025-01-28 15:11:30 -0300

    build

--- frontend/src/components/Header_log.js
+++ frontend/src/components/Header_log.js
@@ -4,6 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import UnitSelector from './UnitSelector';
 import LogoutButton from './LogoutButton';
 import './Header_log.css'; // Importando estilos específicos do Header
+import logoLeft from '../assets/01.png'; // Importe a primeira imagem
+import logoRight from '../assets/02.jpeg'; 
+import logoIdecace  from '../assets/img/logo_idecace_edited.avif';
+
+
 
 function Header({ selectedUnit, handleUnitChange, handleLogout }) {
   const navigate = useNavigate();
@@ -11,6 +16,9 @@ function Header({ selectedUnit, handleUnitChange, handleLogout }) {
   return (
     <header className="header-container">
       <h1 className="header-title">Relatório de Pesquisa</h1>
+      <img src={logoLeft} alt="Logo Esquerda" className="header-logo left" />
+          <img src={logoIdecace}/>
+            <img src={logoRight} alt="Logo Direita" className="header-logo right" />
       <div className="header-actions">
         <UnitSelector selectedUnit={selectedUnit} handleUnitChange={handleUnitChange} />
         <button
